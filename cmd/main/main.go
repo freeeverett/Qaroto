a7a3c44e91ff9fabfb764d85faa7e65da541e9e6
Everett <freeeverettmail@gmail.com>
2025-09-14 00:20:08 +0800

    first commit

---
+++ cmd/main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	// start
+	a := app.New()
+	w := a.NewWindow("qaroto")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
